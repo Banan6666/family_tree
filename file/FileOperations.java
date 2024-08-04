5c5e8a83433793fbcbeccb8aba84685d82a24423
Banan6666 <153874883+Banan6666@users.noreply.github.com>
2024-08-04 23:52:57 +0300

    Delete file directory

--- file/FileOperations.java
+++
@@ -1,8 +0,0 @@
-package family_tree.file;
-
-import family_tree.model.FamilyTree;
-
-public interface FileOperations {
-    void writeToFile(FamilyTree familyTree, String filePath);
-    FamilyTree readFromFile(String filePath);
-}
