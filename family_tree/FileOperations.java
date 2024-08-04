21134dd729bb0d5a9e429b525e7771ac9ec4f2f2
Banan6666 <153874883+Banan6666@users.noreply.github.com>
2024-08-04 23:58:08 +0300

    Delete family_tree/FileOperations.java

--- family_tree/FileOperations.java
+++
@@ -1,8 +0,0 @@
-package family_tree;
-
-import java.io.Serializable;
-
-public interface FileOperations {
-    void writeToFile(FamilyTree familyTree, String filePath);
-    FamilyTree readFromFile(String filePath);
-}
